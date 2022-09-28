fbf9a8230918c2e76a1f367973eb34b7472b2e13
lbk209 <80578322+lbk209@users.noreply.github.com>
2022-09-29 00:43:14 +0900

    Add files via upload

---
+++ myarimagarch.r
@@ -0,0 +1,436 @@
+# ARIMA+GARCH Strategy - fit & predict
+ag.forecast <- function(ret, foreLength, windowLength, lookahead) {
+
+    # Create vectors to store the predictions
+    fc.pred <- vector(mode="numeric", length=foreLength)
+    fc.sigma <- vector(mode="numeric", length=foreLength)
+    fc.dates <- vector(mode="character", length=foreLength)
+    
+    # step to print output
+    print.step <- round((foreLength+1)/5)
+
+    start_time <- Sys.time()
+
+    for (d in 1:(foreLength+1)) {
+
+        # Obtain the S&P500 rolling window for this day
+        retOffset <- ret[d:(windowLength+d)]
+
+        # Fit the ARIMA model
+        fit <- tryCatch(auto.arima(retOffset, seasonal=FALSE, 
+                                   ic='aicc', 
+                                   #ic='aic', 
+                                   d=0, 
+                                   trace=FALSE),
+                        error=function(err) {FALSE},
+                        warning=function(err) {FALSE} )
+
+        if( !is.logical(fit) ) {
+            final.order <- as.numeric(arimaorder(fit))
+        } else {
+            final.order <- c(0,0,0)
+        }
+
+        # Specify and fit the GARCH model
+        spec <- ugarchspec(
+            variance.model=list(garchOrder=c(1,1)),
+            mean.model=list(armaOrder=c(final.order[1], final.order[3]), include.mean=T),
+            distribution.model="sged"
+        )
+        fit <- tryCatch(ugarchfit(spec, retOffset, solver='hybrid'), 
+                        error=function(e) {e}, 
+                        warning=function(w) {w})
+
+        # If the GARCH model does not converge, set the direction to "long" else
+        # choose the correct forecast direction based on the returns prediction
+        # Output the results to the screen and the forecasts vector
+        if(is(fit, "warning")) {
+            fc.pred[d] <- 0 # assume zero return
+            fc.sigma[d] <- 0
+            fc.dates[d] <- as.character(index(tail(retOffset, 1)[1]))
+        } else {
+            fore <- ugarchforecast(fit, n.ahead=1)
+            ind <- fore@forecast$seriesFor
+            sig <- fore@forecast$sigmaFor
+            # save d + lookahead prediction at d date
+            fc.pred[d] <- ind[1]
+            fc.sigma[d] <- sig[1]
+            fc.dates[d] <- colnames(ind)
+        }
+
+        if ((d %% print.step == 0) | (d == foreLength+1)) {
+            a <- Sys.time() - start_time
+            u <- attr(a, "units")
+            v <- as.numeric(a)
+            print(sprintf("%0.0f %% done (%0.0f %s).", 100 * d/(foreLength+1), v, u))
+        }
+    }
+
+    pred <- xts(cbind(fc.pred, fc.sigma), order.by=as.Date(fc.dates, "%Y-%m-%d"))
+    colnames(pred) <- c(paste('series T+', lookahead, sep=''), paste('sigma T+', lookahead, sep=''))
+    return(pred)
+}
+
+
+# ARIMA+GARCH Strategy - plot the result of ag.forecast
+# pred: return value of ag.forecast
+ag.plotforecast <- function(pred, trueCurve, lookahead, 
+                            label='S&P 500', n_sigma=2) 
+{
+    trueCurve <- trueCurve[index(pred)]
+    predSer <- pred[,1]
+    predSig <- pred[,2]
+
+    getPrice <- function(x) exp(x) * trueCurve # calc returns
+    z <- getPrice(predSer)
+    zup <- getPrice(predSer + n_sigma*predSig)
+    zdn <- getPrice(predSer - n_sigma*predSig)
+
+    # merge forecast & true values
+    result <- merge(z, zup)
+    result <- merge(result, zdn)
+    result <- lag(result, lookahead) # shift prediction by lookahead
+    result <- merge(result, trueCurve, all=T)
+    result <- result[!is.na(result[,1])]
+    colnames(result) <- c('Z', 'Zup', 'Zdn', 'price')
+
+    # plot
+    labelPr <- label
+    labelFc <- paste("Forecast w/ conditional ", n_sigma, "-sigma bands", sep="")
+    num_lines <- 2
+    gg <- (ggplot(result, aes(Index, Z))
+             + geom_line(aes(color=labelFc), size = 1)
+             + geom_ribbon(aes(ymin = Zdn, ymax = Zup), fill=hue_pal()(num_lines)[1], alpha=0.1)
+             + geom_line(aes(y=price, color=labelPr), size = 1)
+             + ylab('Index') + xlab("")
+             + theme(legend.position="bottom", legend.title=element_blank())
+             + guides(color=guide_legend(nrow=num_lines))
+    )
+    return(gg)
+}
+
+# ARIMA+GARCH Strategy - trading strategy
+# pred: return value of ag.forecast
+ag.backtest <- function(pred, ret, lookahead) {
+    if (dim(pred)[2]>1) stop("dim of pred is invalid.")
+    
+    # shift prediction to its realized day before merge with ret
+    position <- lag(pred, lookahead)
+    position <- pred[!is.na(position)]
+    # define position
+    position <- ifelse(position < 0, -1, ifelse(position > 0, 1, 0))
+    # merge position with ret
+    position <- merge(position, ret, all=F )
+    # calc result of position
+    result <- position[,1] * position[,2]
+
+    # Create the backtests for ARIMA+GARCH and Buy & Hold
+    # assuming reinvestment of returns at every day
+    # remember return is logarithmic one
+    resArimaGarchCurve = cumsum(result)
+    resBuyHoldCurve = cumsum(position[,2])
+    
+    resCombinedCurve = merge(resArimaGarchCurve, resBuyHoldCurve, all=F )
+
+    # Plot the equity curves
+    xyp <- ag.plot(resCombinedCurve, labels=c("ARIMA+GARCH", "Buy & Hold"))
+    return(xyp)
+}
+
+# see ag.backtest
+ag.plot <- function(data, 
+                    labels=c("ARIMA+GARCH", "Buy & Hold"),
+                    colors=c("darkred", "darkblue")) {
+    xyp <- xyplot( 
+        data,
+        superpose=T,
+        col=colors,
+        lwd=2,
+        key=list(
+            text=list(labels),
+            lines=list(lwd=2, col=colors)
+        )
+    )
+    return(xyp)
+}
+
+
+
+# ARIMA+GARCH Forecasts - fit & predict
+ag2.forecast <- function(x, h, 
+                         mxreg=NULL, mxreg.msize=NULL,
+                         vxreg=NULL, vxreg.msize=NULL,
+                         out.sample=0, order=NULL)
+{
+    
+    getxregh <- function(xreg, xreg.msize) {    
+        if (!is.null(xreg)) {
+            if (is.null(xreg.msize)) {
+                xreg.m <- xreg # calc mean for future with all the xreg
+            } else {
+                # calc mean for future with xreg of length xreg.mszie
+                xreg.m <- tail(xreg, n=xreg.msize)
+            }
+
+            if (is.null(dim(xreg))) {
+                xreg.h <- mean(xreg.m)
+            } else {
+                xreg.h <- colMeans(xreg.m)  
+            }
+
+            xreg.h <- data.frame(xreg.h)
+            colnames(xreg.h) <- colnames(NA)
+            #colnames(xreg.h) <- colnames(xreg) # error with multiple xreg
+            xreg.h <- t(xreg.h)
+            xreg.h <- as.ts(xreg.h[rep(seq_len(nrow(xreg.h)), h), ])
+
+            ## added for single day forecast
+            xreg.coln <- colnames(xreg)
+            xreg.h <- data.frame(xreg.h) # convert to frame for the case of single xreg
+            if ((dim(xreg.h)[2]==1) & (dim(xreg.h)[1]==length(xreg.coln))) {
+                xreg.h <- t(xreg.h) # the case of h==1
+            } else {
+                xreg.h <- as.ts(xreg.h) # convert to ts as xreg is ts
+            }
+        } else {
+            xreg.h <- NULL
+        }
+        return(xreg.h)
+    }
+    mxreg.h <- getxregh(mxreg, mxreg.msize)
+    vxreg.h <- getxregh(vxreg, vxreg.msize)
+    
+    
+    if (is.null(order)) {
+        # get train data for ARIMA model by dropping out-of-sample
+        train <- x[1:(nrow(as.xts(x))-h)]
+        # Fit the ARIMA model
+        fit <- tryCatch(auto.arima(train, seasonal=FALSE, 
+                                   ic='aicc', 
+                                   #ic='aic', 
+                                   d=0, 
+                                   xreg=mxreg, 
+                                   trace=FALSE),
+                        error=function(err) {FALSE},
+                        warning=function(err) {FALSE} )
+
+        if( !is.logical(fit) ) {
+            order <- as.numeric(arimaorder(fit))
+        } else {
+            order <- c(0,0,0)
+        }
+    }
+
+    # Specify and fit the GARCH model
+    spec <- ugarchspec(
+        variance.model=list(garchOrder=c(1,1),
+                            external.regressors=vxreg),
+        mean.model=list(armaOrder=c(order[1], order[3]), include.mean=T,
+                        external.regressors=mxreg),
+        distribution.model="sged"
+    )
+    fit <- tryCatch(ugarchfit(spec, x, solver='hybrid', out.sample=out.sample), 
+                    error=function(e) {e}, 
+                    warning=function(w) {w})
+
+    # If the GARCH model does not converge, set the direction to "long" else
+    # choose the correct forecast direction based on the returns prediction
+    # Output the results to the screen and the forecasts vector
+    if(is(fit, "warning")) {
+        print('GARCH model does not converge')
+        fore <- NA
+    } else {
+        fore <- ugarchforecast(fit, n.ahead=h, 
+                               external.forecasts=list(
+                                   mregfor=mxreg.h, 
+                                   vregfor=vxreg.h),
+                               n.roll=out.sample)
+        
+        setClass(
+          "myuGARCHforecast",
+          contains="uGARCHforecast",
+          slots=c(users="list")
+        ) -> myuGARCHforecast
+        fore <- as(fore, "myuGARCHforecast")
+        fore@users <- list(arima.order=order)
+    }
+    return(fore)
+}
+
+
+# ARIMA+GARCH Forecasts - plot
+# fore: return value of ag2.forecast
+# test: test data to compare with forecast
+ag2.plot <- function(fore, var.mode='unconditional', plot.mode='return',
+                     price=NULL, lookahead=NULL,
+                     figsize=c(10,6),
+                     plotFUN = paste("ag2.plot.garchforecast", 1:2, sep = "."),
+                     test = NULL, test.color='steelblue'
+                    ) 
+{
+    my.figsize(figsize[1],figsize[2])
+    vmode <- ifelse(var.mode=='unconditional', 1, 2)
+    if (vmode == 2) {
+        if (forc@forecast$n.roll < 5) {
+            return('ERROR: Run ag2.forecast with out.sample >= 5')
+        }
+    }
+    
+    if (plot.mode=='return') {
+        plot(fore, which=vmode)
+        if (!is.null(test)) {
+            # reset dates of test same as forecast just for comparison
+            # (forecast dates are given as starting date and length)
+            x <- fore@forecast$sigmaFor
+            x <- as.Date(as.character(attr(x, 'dimnames')[2]))
+            h <- fore@forecast$n.ahead
+            x <- x + 0:h
+
+            y <- tail(fore@model$modeldata$data, 1)
+            y <- append(y, as.numeric(test))
+
+            lines(x, y, col=test.color)
+        }
+    } else {
+        if ((is.null(price) | (is.null(lookahead)))) {
+            return('ERROR: price data & lookahead required.')
+        } else {
+            func <- match.fun(plotFUN[vmode])
+            func(fore, price, lookahead)
+        }
+    }
+}
+
+
+# ARIMA+GARCH Forecasts - plotFUN of ag2.plot in the case of unconditional variance
+ag2.plot.garchforecast.1 <- function(x, price, lookahead,
+                                    size.title=1.0, color.x.sig='grey', 
+                                    ylab='Index',
+                                    #history.length=42,
+                                    history.length=25,
+                                    ...)
+{
+    vmodel = x@model$modeldesc$vmodel
+    # 1. Time Series:
+    #nr = x@forecast$n.roll
+    #if(n.roll > nr) stop("plot-->error: n.roll choice is invalid", call. = FALSE)
+    #n.roll <- nr
+    n.roll <- 0
+    n = x@forecast$n.ahead
+    N = x@forecast$N - x@forecast$n.start
+    forseries = x@forecast$seriesFor[,n.roll+1]
+    forsigma = x@forecast$sigmaFor[,n.roll+1]
+    xdates = x@model$modeldata$index[(N+n.roll-min(N,history.length)):(N+n.roll)]
+    fdates = seq(tail(xdates,1), by = x@model$modeldata$period, length.out = n+1)[-1]
+    series = x@model$modeldata$data[(N+n.roll-min(N,history.length)):(N+n.roll)]
+
+    xforseries = c(series, forseries)
+    series = c(series, rep(NA, n))
+    Zup = forseries+1*forsigma
+    Zdn = forseries-1*forsigma
+    
+    ### added
+    getPrice <- function(x, d) {
+        logret <- as.xts(x, order.by=d)
+        price.init <- tail(price[index(price) < d[1]], lookahead)
+        price.rec <- my.recprice(logret, lookahead, price.init, message=FALSE)
+        return(as.numeric(price.rec))
+    }
+    xforseries <- getPrice(xforseries, c(xdates, fdates))
+    series <- getPrice(series, c(xdates, fdates))
+    forseries <- getPrice(forseries, fdates)
+    Zup <- getPrice(Zup, fdates)
+    Zdn <- getPrice(Zdn, fdates)
+    
+    #ylim=c(0.95*min(xforseries,na.rm=TRUE), 1.05*max(xforseries,na.rm=TRUE))
+    ylim=c(0.99*min(xforseries, Zdn, na.rm=TRUE), 1.01*max(xforseries, Zup, na.rm=TRUE))
+    plot(c(xdates, fdates), as.numeric(xforseries), type="l", col="steelblue",
+            main = paste("Forecast Series\n w/th unconditional 1-Sigma bands", sep = "") ,
+            ylab="Series",xlab="Time/Horizon", 
+            ylim = ylim, 
+            cex.main = 0.7, cex.axis = 0.8, cex.lab = 0.9)
+    abline(h = 0, col = "grey", lty = 3)
+    for(i in 2:n) rect(fdates[i-1], Zdn[i-1], fdates[i], Zup[i], col = colors()[142], border=NA)
+    lines(c(xdates, fdates), series, col="steelblue")
+    lines(fdates, forseries, col="tomato1")
+    mtext(paste("GARCH model : ", vmodel), side = 4, adj = 0, padj=0, col = "gray", cex = 0.5)
+    if(vmodel=="fGARCH"){
+        mtext(paste("fGARCH submodel: ", x@model$modeldesc$vsubmodel, sep = ""), side = 4, adj = 0, padj=1.5, col = "gray", cex = 0.5)
+        mtext(paste("Horizon: ",n,sep=""), side = 3, adj = 0, col = "gray", cex = 0.5)
+    } else{
+        mtext(paste("Horizon: ",n,sep=""), side = 3, adj = 0, col = "gray", cex = 0.5)
+    }
+    lg.txt = c("Actual","Forecast")
+    legend("topleft", legend = lg.txt, col = c("steelblue", "tomato1"), y.intersp = 1.5, pch = 21, cex = 0.7, bty="n")
+    box()
+    grid()
+}
+
+
+# ARIMA+GARCH Forecasts - plotFUN of ag2.plot in the case of conditional variance
+ag2.plot.garchforecast.2 <- function(x, price, lookahead,
+                                    size.title=1.0, color.x.sig='grey', 
+                                    ylab='Index',
+                                    #history.length=42,
+                                    history.length=25,
+                                    ...)
+{
+    vmodel = x@model$modeldesc$vmodel
+    # 1. Time Series:
+    nr = x@forecast$n.roll
+    if(nr<5) stop("\nn.roll less than 5!...does not make sense to provide this plot.")
+    N = x@forecast$N - x@forecast$n.start
+    fdata  = x@forecast$seriesFor[1,]
+    fsigma = x@forecast$sigmaFor[1,]
+    xdata = x@model$modeldata$data[((N+1)-min(N, history.length)):(N+nr)]
+    xsigma = x@model$modeldata$sigma[((N+1)-min(N, history.length)):(N+nr)]
+    xdates = x@model$modeldata$index[((N+1)-min(N, history.length)):(N+nr)]
+    ns = length(xdata)
+    xplus =  xdata + 2*xsigma
+    xminus = xdata - 2*xsigma
+    fplus = c(rep(NA, (ns - nr)), fdata[-length(fdata)] +  2*fsigma[-length(fsigma)])
+    fminus = c(rep(NA, (ns - nr)), fdata[-length(fdata)] - 2*fsigma[-length(fsigma)])
+    fdata = c(rep(NA, (ns - nr)), fdata[-length(fdata)])
+    
+    ### added
+    price.shitfed <- lag(price, lookahead)[xdates]
+    getPrice <- function(x) exp(x) * price.shitfed # calc returns
+    xdata <- getPrice(xdata)
+    xplus <- getPrice(xplus)
+    xminus <- getPrice(xminus)
+    fdata <- getPrice(fdata)
+    fplus <- getPrice(fplus)
+    fminus <- getPrice(fminus)
+
+    #ylim=c(0.95*min(xminus,na.rm=TRUE), 1.2*max(xplus,na.rm=TRUE))
+    ylim=c(0.99*min(xminus, fminus, na.rm=TRUE), 1.01*max(xplus, fplus, na.rm=TRUE))
+    plot(xdates, xdata, type="l", col="black",
+            main = paste("Rolling Forecast vs Actual Series\n w/th conditional 2-Sigma bands", sep = "") ,
+            ylab=ylab,xlab="Time/Horizon", 
+            ylim = ylim, 
+            cex.main = size.title,
+            cex.axis = 0.8, cex.lab = 0.9)
+    for(i in 26:(nr+25)) rect(xdates[i-1], fminus[i-1], xdates[i], fplus[i], col = colors()[142], border=NA)
+    lines(xdates, xdata, col = "steelblue")
+    lines(xdates, xplus, col = color.x.sig, lwd = 0.5)
+    lines(xdates, xminus, col = color.x.sig, lwd = 0.5)
+    abline(h = 0, col = "grey", lty = 3)
+    lines(xdates, fdata, col = "tomato1", lwd = 2.5)
+    #lines(xts(fplus,xdates),  col = "brown", lwd = 0.5)
+    #lines(xts(fminus,xdates),  col = "brown", lwd = 0.5)
+
+    mtext(paste("GARCH model : ", vmodel), side = 4, adj = 0, padj=0, col = "gray", cex = 0.5)
+    if(vmodel=="fGARCH"){
+        mtext(paste("fGARCH submodel: ", x@model$modeldesc$vsubmodel, sep = ""), side = 4, adj = 0, padj=1.5, col = "gray", cex = 0.5)
+        mtext(paste("Horizon: ", nr, sep=""), side = 3, adj = 0, col = "gray", cex = 0.5)
+    } else{
+        mtext(paste("Horizon: ", nr, sep=""), side = 3, adj = 0, col = "gray", cex = 0.5)
+    }
+    lg.txt = c("Actual","Forecast")
+    legend("topleft", legend = lg.txt, col = c("steelblue", "tomato1"), y.intersp = 1.5, pch = 21, cex = 0.7, bty="n")
+    box()
+    grid()
+}
+
+
+
---
+++ myutils.r
@@ -0,0 +1,341 @@
+library(repr)
+library(forecast)
+library(ggplot2)
+
+
+my.figsize <- function(w, h) {
+    if(missing(w)) {
+        x <- getOption("repr.plot.width")
+        y <- getOption("repr.plot.height")
+        sprintf("width: %s, height %s", x, y)
+    } else {
+        options(repr.plot.width=w, repr.plot.height=h)
+    }
+}
+
+
+# fit: (result) value of Arima in forecast pkg
+# future: number of times to forecast
+# past: number of past data to plot with forecast
+# test: actual data to compare with forecasts
+my.plot_forecast <- function(fit, future=NULL, past=NULL, test=NULL, xreg=NULL) {
+    
+    series <- eval(parse(text=fit$series))
+    l <- length(series)
+    
+    if (is.null(future)) {
+        future <- as.integer(0.1 * l)
+    }
+    
+    if (is.null(past)) {
+        past <- l
+    }
+    
+    y <- forecast(fit, h=future, xreg=xreg)
+    gg <- autoplot(y, include=past)
+    
+    if (!is.null(test)) {
+        if (!is.ts(test)) {
+            test <- ts(as.numeric(test), start=start(y$mean)[1], end=end(y$mean)[1])
+        }
+        gg <- gg +
+               autolayer(test, series="Actuals") +
+               autolayer(y$mean, series="Forecasts")
+    }
+    return(gg)
+}
+
+
+# updated from following source
+# https://stats.stackexchange.com/questions/431545/why-isnt-the-tscv-function-allowing-for-step-size-other-than-1
+my.tsCV <- function (y, forecastfunction, h = 1, window = NULL, xreg = NULL, 
+          initial = 0, step = 1, silent=TRUE, count.freq=0.1, ...) 
+{
+    y <- as.ts(y)
+    n <- length(y)
+    step <- round(step)
+    step_ind <- seq(step, n - 1L, by = step) ### Added line
+
+    if (initial >= n) 
+        stop("initial period too long")
+
+    if (!is.null(xreg)) {
+        xreg <- ts(as.matrix(xreg))
+        if (NROW(xreg) != length(y)) 
+            stop("xreg must be of the same size as y")
+        tsp(xreg) <- tsp(y)
+    }
+    
+    if (is.null(window)) {
+        indx <- seq(1 + initial, n - 1L)
+    } else {
+        indx <- seq(window + initial, n - 1L, by = 1L)
+    }
+    indx <- intersect(indx, step_ind) ### Added line
+    
+    e.cols <- c('forecast_start', 'forecast_end', 'rmse', 'mape')
+    e <- ts(matrix(NA_real_, nrow = floor(n/step), ncol = length(e.cols)))
+    colnames(e) <- e.cols
+    
+    indx.len <- length(indx)
+    by <- round(count.freq*indx.len)
+    by <- max(1, by)
+    print.when <- seq(0, indx.len, by=by)
+    
+    cnt <- 0
+    for (i in indx) {
+		# get new start of subset of y & xreg
+    	if (is.null(window)) {
+		    start <- 1L
+		} else {
+		    if (i - window >= 0L) {
+		        start <- i - window + 1L
+		    } else {
+		        stop("small window")
+		    }
+		}
+		
+		y_subset <- subset(y, start=start, end = i)
+		if (is.null(xreg)) {
+		    fc <- try(suppressWarnings(forecastfunction(y_subset, 
+		                                          h = h, ...)), silent = silent)
+		    #                                      h = h)), silent = silent)
+		} else {
+		    xreg_subset <- as.matrix(subset(xreg, start=start, end=i))
+		    fc <- try(suppressWarnings(forecastfunction(y_subset, 
+		                                          h = h, xreg = xreg_subset, ...)), silent = silent)
+		    #                                      h = h, xreg = xreg_subset)), silent = silent)
+		}
+        if (!is.element("try-error", class(fc))) {
+            rmse <- sqrt(mean((fc$mean - y[i + (1:h)])^2))
+            mape <- mean(abs(1 - fc$mean / y[i + (1:h)]))
+            e[i/step, ] <- c(i+1, i+h, rmse, mape)
+        }
+        
+        cnt <- cnt + 1
+        if (cnt %in% print.when) {
+            #print(sprintf("%0.0f %% done.", 100*cnt/length(indx)))
+            message(sprintf("%0.0f %% done.", 100*cnt/length(indx)))
+            #cat(sprintf("%0.0f %% done.", 100*cnt/length(indx)))
+        }
+    }
+    #return(na.omit(e)) # times of NA kept in e as attr(na.action)
+    return(e)
+}
+
+
+# type 2 of time series cross validation 
+# forecastfunction.2 returns avg and std of errors
+my.tsCV.2 <- function (y, forecastfunction.2, h = 1, window = NULL, xreg = NULL, 
+          initial = 0, step = 1, silent=TRUE, count.freq=0.1, ...) 
+{
+    y <- ts(y, start=1, frequency=1) # converted to n * 1 ts
+    n <- length(y)
+    step <- round(step)
+    step_ind <- seq(step, n - 1L, by = step) ### Added line
+
+    if (initial >= n) 
+        stop("initial period too long")
+
+    if (!is.null(xreg)) {
+        xreg <- ts(xreg, start=1, frequency=1) # work even if n * 1?
+        if (NROW(xreg) != length(y)) 
+            stop("xreg must be of the same size as y")
+        tsp(xreg) <- tsp(y)
+    }
+    
+    if (is.null(window)) {
+        indx <- seq(1 + initial, n - 1L)
+    } else {
+        indx <- seq(window + initial, n - 1L, by = 1L)
+    }
+    indx <- intersect(indx, step_ind) ### Added line
+    
+    e.cols <- c('forecast_start', 'forecast_end', 
+                'rmse.mean', 'rmse.sigma', 'mape.mean', 'mape.sigma')
+    e <- ts(matrix(NA_real_, nrow = floor(n/step), ncol = length(e.cols)))
+    colnames(e) <- e.cols
+    
+    indx.len <- length(indx)
+    by <- round(count.freq*indx.len)
+    by <- max(1, by)
+    print.when <- seq(0, indx.len, by=by)
+    
+    cnt <- 0
+    for (i in indx) {
+        # get new start of subset of y & xreg
+        if (is.null(window)) {
+            start <- 1L
+        } else {
+            if (i - window >= 0L) {
+                start <- i - window + 1L
+            } else {
+                stop("small window")
+            }
+        }
+        if (i+h > length(y)) {
+            break
+        } else {
+            y_subset <- window(y, start, i+h)
+        }
+        if (is.null(xreg)) {
+            fc <- try(suppressWarnings(forecastfunction.2(y_subset, 
+                                                  h = h, ...)), silent = silent)
+            #                                      h = h)), silent = silent) # uncomment when debugging this function
+        } else {
+            xreg_subset <- window(xreg, start, i+h)
+            fc <- try(suppressWarnings(forecastfunction.2(y_subset, 
+                                                  h = h, xreg = xreg_subset, ...)), silent = silent)
+            #                                      h = h, xreg = xreg_subset)), silent = silent) # uncomment when debugging this function
+        }
+        if (!is.element("try-error", class(fc))) {
+            e[i/step, ] <- c(i+1, i+h, fc$rmse.mean, fc$rmse.sigma, fc$mape.mean, fc$mape.sigma)
+        }
+        
+        cnt <- cnt + 1
+        if (cnt %in% print.when) {
+            print(sprintf("%0.0f %% done.", 100*cnt/length(indx)))
+        }
+    }
+    return(e)
+}
+
+
+
+# x: xts/ts obj
+my.minmaxscale <- function(x, center=NULL, scale=NULL) {
+
+	x <- na.omit(x)
+	
+	if (!is.null(center)) { # inv.transf if the args given
+		sc <- center
+		ss <- scale	
+	} else if (!is.null(attr(x, 'scaled:scale'))) { # inv.transf if the attr given
+		sc <- attr(x, 'scaled:center')
+		ss <- attr(x, 'scaled:scale')	
+	} else { # scale
+	    sc <- NULL
+	}
+
+	if ('xts' %in% class(x)) {
+		# set time zone same as that of x
+		# as the apply function will lose the time zone
+		tz <- tzone(x)
+		Sys.setenv(TZ = tz)
+		as.time <- function(x) {as.xts(x, tz=tz)}
+	} else {
+		x <- as.xts(x)
+		as.time <- as.ts
+	}
+
+	# check if single time series
+	if (is.null(dim(x))) {
+		TO.TS <- TRUE
+	} else if (dim(x)[2] == 1) {
+		TO.TS <- TRUE
+	} else {
+		TO.TS <- FALSE
+	}
+
+	# save col names
+	cols <- colnames(x)
+
+
+	# transform or inv.
+	if (is.null(sc)) { # transform
+		sc <- apply(x, 2, mean)
+		ss <- (apply(x, 2, max) - apply(x, 2, min)) / 2
+		x <- apply(x, 1, function(r) {(r - sc) / ss})
+	} else { # inv.transform
+		# arrts reset
+		x <- apply(x, 1, function(r) {r * ss + sc})
+		sc <- NULL
+	}
+
+	# convert to xts with original time zone
+	if (TO.TS) {
+		x <- as.time(x) 
+	} else {
+		x <- as.time(t(x))
+	}
+	colnames(x) <- cols
+	if (!is.null(sc)) {
+		attr(x, 'scaled:center') <- sc
+		attr(x, 'scaled:scale') <- ss
+	}
+	return(x)
+}
+
+
+# get price from log. return
+# logret: logret of period
+# period: period for logret
+# prices.init: init. prices for period before first logret
+my.recprice <- function(logret, period, prices.init, message=TRUE) {
+    N <- nrow(logret)
+    price <- NULL
+    for (i in 1:period) {
+        logret.sum <- tryCatch(logret[seq(i, N, by=period)],
+         					   error=function(e) {
+                                         if (message) {print(e)}
+                                         # use NULL instead of NA to avoid error (the condition has length > 1)
+                                         # if logret.sum is series
+                                         return(NULL)
+                                     }
+                              )
+        if (is.null(logret.sum)) next 
+        price.rec <- rep(prices.init[i], nrow(logret.sum)) * exp(cumsum(logret.sum))
+        if (is.null(price)) {
+            price <- price.rec
+        } else {
+            price <- rbind(price, price.rec)
+        }
+    }
+    return(price)
+}
+
+
+my.get_result <- function(x, group, errors=c('rmse','mape'), group.col='cs') {
+    x <- x[,errors]
+    x <- as.data.frame(x)
+    x <- na.omit(x)
+    x[group.col] <- group
+    return(x)
+}
+
+
+my.plot_errors <- function(results, metrics=c('rmse','mape'), group.col='cs', loc='right',
+                           ylog=FALSE) 
+{
+    func <- function(x, q, l){
+        y <- (quantile(x, probs=q) + median(x))*.5
+        return(data.frame(y = y, label = l)) 
+    }
+    if (ylog) {
+        func.m <- function(x) {func(x, 0.75, signif(mean(10^x), 3))}
+    } else {
+        func.m <- function(x) {func(x, 0.75, signif(mean(x), 3))}
+    }
+    func.n <- function(x) {func(x, 0.25, paste('n', length(x), sep='='))}
+
+    p <- NULL
+    for (m in metrics) {
+        p.m <- (ggplot(results, aes_string(group.col, m, fill=group.col)) 
+                + geom_boxplot()
+                + theme(legend.position=loc)
+                + stat_summary(fun.data=func.m, fun=median, geom="text")
+                + stat_summary(fun.data=func.n, fun=median, geom="text")
+        )
+        if (is.null(p)) {
+            p <- p.m
+        } else {
+            p <- p + p.m
+        }
+    }
+    if (ylog) {
+            p <- (p + scale_y_continuous(trans='log10') 
+                    + labs(y = paste(p$labels$y, " (log scale)")))
+    }
+    return(p)
+}
+
